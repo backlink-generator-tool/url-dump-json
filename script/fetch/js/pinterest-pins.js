cd478f1ab7eb4c2fc0d03367ed12990c057da463
Backlink Generator Tool – Free SEO Backlink Builder <backlink.generator.tool@gmail.com>
2025-12-04 23:49:25 +0100

    Update pinterest-pins.js

--- script/fetch/js/pinterest-pins.js
+++ script/fetch/js/pinterest-pins.js
@@ -1,6 +1,6 @@
 // examble-build-pinterest.module.js
 // ES module: export default an array of fully-formed URLs (no global)
-const fetchData  = [
+const PIN_IDS = [
     "615656211566717661",
   	"615656211564500126",
   	"615656211564500024",
@@ -21,60 +21,60 @@ const fetchData  = [
 ];
 
 const PINTEREST_DOMAINS = [
-    "https://www.pinterest.com/",
-    "https://www.pinterest.at/",
-    "https://www.pinterest.be/",
-    "https://www.pinterest.ca/",
-    "https://www.pinterest.cl/",
-    "https://www.pinterest.co/",
-    "https://www.pinterest.dk/",
-    "https://www.pinterest.ec/",
-    "https://www.pinterest.fr/",
-    "https://www.pinterest.de/",
-    "https://www.pinterest.hu/",
-    "https://www.pinterest.in/",
-    "https://www.pinterest.id/",
-    "https://www.pinterest.ie/",
-    "https://www.pinterest.it/",
-    "https://www.pinterest.jp/",
-    "https://www.pinterest.mx/",
-    "https://www.pinterest.nl/",
-    "https://www.pinterest.pe/",
-    "https://www.pinterest.nz/",
-    "https://www.pinterest.pt/",
-    "https://www.pinterest.ph/",
-    "https://www.pinterest.ru/",
-    "https://www.pinterest.kr/",
-    "https://www.pinterest.se/",
-    "https://www.pinterest.es/",
-    "https://www.pinterest.ch/",
-    "https://www.pinterest.tw/",
-    "https://www.pinterest.uk/",
-    "https://www.pinterest.th/",
-    "https://www.pinterest.vn/",
-    "https://www.pinterest.co.uk/",
-    "https://www.pinterest.co.kr/",
-    "https://www.pinterest.com.au/",
-    "https://www.pinterest.com.mx/",
-    "https://ar.pinterest.com/",
-    "https://uk.pinterest.com/",
-    "https://fr.pinterest.com/",
-    "https://de.pinterest.com/",
-    "https://es.pinterest.com/",
-    "https://it.pinterest.com/",
-    "https://au.pinterest.com/",
-    "https://dk.pinterest.com/",
-    "https://pt.pinterest.com/",
-    "https://ru.pinterest.com/",
-    "https://jp.pinterest.com/",
-    "https://cl.pinterest.com/",
-    "https://co.pinterest.com/",
-    "https://nl.pinterest.com/",
-    "https://in.pinterest.com/",
-    "https://kr.pinterest.com/"
+	"https://www.pinterest.com/pin/",
+    "https://www.pinterest.at/pin/",
+    "https://www.pinterest.be/pin/",
+    "https://www.pinterest.ca/pin/",
+    "https://www.pinterest.cl/pin/",
+    "https://www.pinterest.co/pin/",
+    "https://www.pinterest.dk/pin/",
+    "https://www.pinterest.ec/pin/",
+    "https://www.pinterest.fr/pin/",
+    "https://www.pinterest.de/pin/",
+    "https://www.pinterest.hu/pin/",
+    "https://www.pinterest.in/pin/",
+    "https://www.pinterest.id/pin/",
+    "https://www.pinterest.ie/pin/",
+    "https://www.pinterest.it/pin/",
+    "https://www.pinterest.jp/pin/",
+    "https://www.pinterest.mx/pin/",
+    "https://www.pinterest.nl/pin/",
+    "https://www.pinterest.pe/pin/",
+    "https://www.pinterest.nz/pin/",
+    "https://www.pinterest.pt/pin/",
+    "https://www.pinterest.ph/pin/",
+    "https://www.pinterest.ru/pin/",
+    "https://www.pinterest.kr/pin/",
+    "https://www.pinterest.se/pin/",
+    "https://www.pinterest.es/pin/",
+    "https://www.pinterest.ch/pin/",
+    "https://www.pinterest.tw/pin/",
+    "https://www.pinterest.uk/pin/",
+    "https://www.pinterest.th/pin/",
+    "https://www.pinterest.vn/pin/",
+    "https://www.pinterest.co.uk/pin/",
+    "https://www.pinterest.co.kr/pin/",
+    "https://www.pinterest.com.au/pin/",
+    "https://www.pinterest.com.mx/pin/",
+    "https://ar.pinterest.com/pin/",
+    "https://uk.pinterest.com/pin/",
+    "https://fr.pinterest.com/pin/",
+    "https://de.pinterest.com/pin/",
+    "https://es.pinterest.com/pin/",
+    "https://it.pinterest.com/pin/",
+    "https://au.pinterest.com/pin/",
+    "https://dk.pinterest.com/pin/",
+    "https://pt.pinterest.com/pin/",
+    "https://ru.pinterest.com/pin/",
+    "https://jp.pinterest.com/pin/",
+    "https://cl.pinterest.com/pin/",
+    "https://co.pinterest.com/pin/",
+    "https://nl.pinterest.com/pin/",
+    "https://in.pinterest.com/pin/",
+    "https://kr.pinterest.com/pin/"
 ];
 
-const fetchURLs  = [
+const fetchIds = [
     "https://backlink-generator-tool.github.io/url-dump-json/url/pinterest-pins/pin-1.json",
   	"https://backlink-generator-tool.github.io/url-dump-json/url/pinterest-pins/pin-2.json",
   	"https://backlink-generator-tool.github.io/url-dump-json/url/pinterest-pins/pin-3.json",
@@ -95,16 +95,16 @@ const fetchURLs  = [
 function shuffle(array){ for(let i=array.length-1;i>0;i--){const j=Math.floor(Math.random()*(i+1));[array[i],array[j]]=[array[j],array[i]];} }
 
 function updateDefaultURLs() {
-  shuffle(fetchURLs );
-  for (const u of fetchURLs) {
+  shuffle(fetchIds);
+  for (const u of fetchIds) {
     try {
       console.log("🔎 Trying to fetch URL list from:", u);
       const r = await fetch(u);
       if (!r.ok) throw new Error("HTTP " + r.status);
       const data = await r.json();
       if (Array.isArray(data) && data.length) {
-        fetchData.length = 0;
-        fetchData.push(...data);
+        PIN_IDS.length = 0;
+        PIN_IDS.push(...data);
         console.log("✅ Loaded", data.length, "URLs from", u);
         return;
       }
@@ -117,9 +117,9 @@ function updateDefaultURLs() {
 updateDefaultURLs();
 
 const urls = [];
-for (const id of fetchData) {
+for (const id of PIN_IDS) {
   for (const d of PINTEREST_DOMAINS) {
-    urls.push(d + "pin/" + id);
+    urls.push(d + id);
   }
 }
 
